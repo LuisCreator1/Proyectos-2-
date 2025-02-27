842ee314e237244232097dcea62974eb01dda4cc
LuisAngelMartinezBotello <lamartinezbotello@gmail.com>
2025-02-27 18:54:09 -0400

    Add files via upload

---
+++ DataGridView/DataGridView/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace DataGridView
+{
+    partial class Forms
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dtgvProductos = new System.Windows.Forms.DataGridView();
+            Codigo = new DataGridViewTextBoxColumn();
+            Nombre = new DataGridViewTextBoxColumn();
+            Precio = new DataGridViewTextBoxColumn();
+            lblCodigo = new Label();
+            txtCodigo = new TextBox();
+            txtNombre = new TextBox();
+            lblNombre = new Label();
+            txtPrecio = new TextBox();
+            lblPrecio = new Label();
+            lblInformacion = new Label();
+            btnAdicionar = new Button();
+            btnBorrar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dtgvProductos).BeginInit();
+            SuspendLayout();
+            // 
+            // dtgvProductos
+            // 
+            dtgvProductos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtgvProductos.Columns.AddRange(new DataGridViewColumn[] { Codigo, Nombre, Precio });
+            dtgvProductos.Location = new Point(12, 281);
+            dtgvProductos.Name = "dtgvProductos";
+            dtgvProductos.RowHeadersWidth = 51;
+            dtgvProductos.Size = new Size(532, 244);
+            dtgvProductos.TabIndex = 0;
+            dtgvProductos.CellClick += dtgvProductos_CellClick;
+            // 
+            // Codigo
+            // 
+            Codigo.HeaderText = "Codigo Producto";
+            Codigo.MinimumWidth = 6;
+            Codigo.Name = "Codigo";
+            Codigo.Width = 125;
+            // 
+            // Nombre
+            // 
+            Nombre.HeaderText = "Nombre Producto";
+            Nombre.MinimumWidth = 6;
+            Nombre.Name = "Nombre";
+            Nombre.Width = 125;
+            // 
+            // Precio
+            // 
+            Precio.HeaderText = "Precio Producto";
+            Precio.MinimumWidth = 6;
+            Precio.Name = "Precio";
+            Precio.Width = 125;
+            // 
+            // lblCodigo
+            // 
+            lblCodigo.AutoSize = true;
+            lblCodigo.Location = new Point(12, 26);
+            lblCodigo.Name = "lblCodigo";
+            lblCodigo.Size = new Size(58, 20);
+            lblCodigo.TabIndex = 1;
+            lblCodigo.Text = "Codigo";
+            // 
+            // txtCodigo
+            // 
+            txtCodigo.Location = new Point(76, 23);
+            txtCodigo.Name = "txtCodigo";
+            txtCodigo.Size = new Size(125, 27);
+            txtCodigo.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(76, 67);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(125, 27);
+            txtNombre.TabIndex = 4;
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(12, 70);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(64, 20);
+            lblNombre.TabIndex = 3;
+            lblNombre.Text = "Nombre";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(76, 115);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(125, 27);
+            txtPrecio.TabIndex = 6;
+            // 
+            // lblPrecio
+            // 
+            lblPrecio.AutoSize = true;
+            lblPrecio.Location = new Point(12, 118);
+            lblPrecio.Name = "lblPrecio";
+            lblPrecio.Size = new Size(50, 20);
+            lblPrecio.TabIndex = 5;
+            lblPrecio.Text = "Precio";
+            // 
+            // lblInformacion
+            // 
+            lblInformacion.AutoSize = true;
+            lblInformacion.Location = new Point(12, 201);
+            lblInformacion.Name = "lblInformacion";
+            lblInformacion.Size = new Size(50, 20);
+            lblInformacion.TabIndex = 7;
+            lblInformacion.Text = "label1";
+            // 
+            // btnAdicionar
+            // 
+            btnAdicionar.Location = new Point(261, 22);
+            btnAdicionar.Name = "btnAdicionar";
+            btnAdicionar.Size = new Size(94, 29);
+            btnAdicionar.TabIndex = 8;
+            btnAdicionar.Text = "Adicionar";
+            btnAdicionar.UseVisualStyleBackColor = true;
+            btnAdicionar.Click += btnAdicionar_Click;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.Location = new Point(261, 65);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(94, 29);
+            btnBorrar.TabIndex = 9;
+            btnBorrar.Text = "Borrar";
+            btnBorrar.UseVisualStyleBackColor = true;
+            btnBorrar.Click += btnBorrar_Click;
+            // 
+            // Forms
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(556, 537);
+            Controls.Add(btnBorrar);
+            Controls.Add(btnAdicionar);
+            Controls.Add(lblInformacion);
+            Controls.Add(txtPrecio);
+            Controls.Add(lblPrecio);
+            Controls.Add(txtNombre);
+            Controls.Add(lblNombre);
+            Controls.Add(txtCodigo);
+            Controls.Add(lblCodigo);
+            Controls.Add(dtgvProductos);
+            Name = "Forms";
+            Text = "Forms";
+            Load += Form1_Load;
+            Click += Forms_Click;
+            ((System.ComponentModel.ISupportInitialize)dtgvProductos).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgvProductos;
+        private DataGridViewTextBoxColumn Codigo;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Precio;
+        private Label lblCodigo;
+        private TextBox txtCodigo;
+        private TextBox txtNombre;
+        private Label lblNombre;
+        private TextBox txtPrecio;
+        private Label lblPrecio;
+        private Label lblInformacion;
+        private Button btnAdicionar;
+        private Button btnBorrar;
+    }
+}
---
+++ DataGridView/DataGridView/Form1.cs
@@ -0,0 +1,55 @@
+namespace DataGridView
+{
+    public partial class Forms : Form
+    {
+        private int n = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnaAdicionar_Click(object sender, EventArgs e)
+        {
+            int n = dtgvProductos.Rows.Add();
+
+
+            dtgvProductos.Rows[n].Cells[0].Value = txtCodigo.Text;
+            dtgvProductos.Rows[n].Cells[1].Value = txtNombre.Text;
+            dtgvProductos.Rows[n].Cells[2].Value = txtPrecio.Text;
+
+
+            txtCodigo.Text = string.Empty;
+            txtNombre.Text = string.Empty;
+            txtPrecio.Text = string.Empty;
+        }
+
+        private void dtgvProductos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            if (e.RowIndex != -1)
+            {
+                lblInformacion.Text = (string)dtgvProductos.Rows[n].Cells[1].Value;
+
+            }
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (n != -1)
+            {
+                dtgvProductos.Rows.RemoveAt(n);
+            }
+        }
+    }
+}
