a1db5e34920c81f564f8d94d44020ab1e3bbe7c2
LuisAngelMartinezBotello <lamartinezbotello@gmail.com>
2025-03-21 12:10:11 -0400

    Add files via upload

---
+++ Utilizar LINQ en archivos XML en C#/Utilizar LINQ en archivos XML en C#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string path = "archivo.xml";
+        XElement storeXml = XElement.Load(path);
+
+        var beers = from b in storeXml.Element("beers").Elements("beer")
+                    orderby (string)b.Attribute("style")
+                    select new Beer
+                    {
+                        Name = (string)b.Value,
+                        Style = (string)b.Attribute("style")
+                    };
+
+        foreach (var beer in beers)
+        {
+            Console.WriteLine($"{beer.Name} - {beer.Style}");
+        }
+    }
+}
+
+class Beer
+{
+    public string Name { get; set; }
+    public string Style { get; set; }
+}
+
