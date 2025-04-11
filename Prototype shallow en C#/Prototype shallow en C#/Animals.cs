6d4000cd532cbb8e33518757b71d1c10ee879f0b
LuisAngelMartinezBotello <lamartinezbotello@gmail.com>
2025-04-10 21:13:11 -0400

    Add files via upload

---
+++ Prototype shallow en C#/Prototype shallow en C#/Animals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_shallow_en_C_
+{
+    public class Animal : ICloneable
+    {
+        public int Patas { get; set; }
+        public string Nombre { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+
+}
---
+++ Prototype shallow en C#/Prototype shallow en C#/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_shallow_en_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal oAnimal = new Animal() { Nombre = "Oveja Dolly", Patas = 4 };
+            Animal oAnimalClonado = oAnimal.Clone() as Animal;
+            oAnimalClonado.Patas = 5;
+
+            Console.WriteLine(oAnimal.Patas);
+        }
+
+    }
+}
+
+            
+    
