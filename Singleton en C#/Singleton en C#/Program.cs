2b75a57e6f19edaa49e15c75a4715bdfa5f42931
LuisAngelMartinezBotello <lamartinezbotello@gmail.com>
2025-04-10 21:12:00 -0400

    Add files via upload

---
+++ Singleton en C#/Singleton en C#/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine(Singleton.Instance.mensaje);
+
+        
+            Singleton.Instance.mensaje = "Cambio de mensaje";
+
+       
+            Console.WriteLine(Singleton.Instance.mensaje);
+
+            
+        }
+    }
+}
---
+++ Singleton en C#/Singleton en C#/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        public string mensaje;
+
+        // Constructor privado
+        private Singleton()
+        {
+            mensaje = "Hola mundo";
+        }
+
+        // Propiedad pública para acceder a la instancia
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+    }
+}
